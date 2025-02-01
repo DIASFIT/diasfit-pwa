3f32ee970270573f1b7d1fa6a9367944b95fe866
DIASFIT <suportediasfit@gmail.com>
2025-02-01 18:55:55 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,35 @@
-self.addEventListener('install', function(event) {
+const CACHE_NAME = 'diasfit-cache-v1';
+
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('diasfit-cache').then(function(cache) {
+        caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/icon-192.png',
-                '/icon-512.png'
+                '/', // Página inicial
+                '/index.html', // Se existir uma página principal estática
+                'https://diasfit.github.io/diasfit-pwa/manifest.json'
             ]);
         })
     );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
+        caches.match(event.request).then((response) => {
             return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
         })
     );
 });
